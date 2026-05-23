80b40410c2db3ff60b1237a33fa993bf53271668
coder-minhazulalam <minhazulalam409@gmail.com>
2026-05-24 04:05:22 +0600

    deploy issues

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,28 +1,34 @@
-import { betterAuth } from 'better-auth';
-import { mongodbAdapter } from 'better-auth/adapters/mongodb';
-import { MongoClient } from 'mongodb';
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.AUTH_DB_URI);
 const db = client.db("NewsPortalRegister");
 
 export const auth = betterAuth({
-    emailAndPassword: { 
-    enabled: true, 
-   },
-       socialProviders: {
-        google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID , 
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET , 
-        }, 
-                github: { 
-            clientId: process.env.GITHUB_CLIENT_ID , 
-            clientSecret: process.env.GITHUB_CLIENT_SECRET, 
-        }, 
+  emailAndPassword: {
+    enabled: true,
+  },
+
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     },
 
-        
-  database: mongodbAdapter(db, {  
-    client
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    },
+  },
+
+  // add this
+  trustedOrigins: [
+    "http://localhost:3000",
+    process.env.BETTER_AUTH_URL,
+  ],
+
+  database: mongodbAdapter(db, {
+    client,
   }),
 });
-
